bd29241e8aedf5f216b4db988c56ed863ec4b65b
root <root@tim-Home.(none)>
2013-05-29 20:02:05 -0400

    fresh kernel

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 8 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#8 SMP PREEMPT Wed May 29 18:15:31 EDT 2013"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "tim-Home"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-202) "
